88676f82e2f4e0442db63bcdfd883c0010d68c4f
Luis Rodriguez <lrodriguez@sciodev.com>
2020-02-17 16:38:52 -0600

    fixed authorization call

--- Training/Startup.cs
+++ Training/Startup.cs
@@ -105,7 +105,7 @@ namespace Training
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseAuthentication();
             app.UseErrorHandlingMiddleware();
 
             app.UseHttpsRedirection();
